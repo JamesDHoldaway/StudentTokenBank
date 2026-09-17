b7a04349b7f1760080390eaf080baa4dc6b4a259
JamesDHoldaway <jdholdaw@ncsu.edu>
2026-09-17 09:36:19 -0400

    Added remove student option that requires confirmation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bank import students, apply_rule, save_students
 print(students)
 
 while True:
-    print("\n1. Apply a rule\n2. View a student's balance\n3. List all students\n4. Quit")
+    print("\n1. Apply a rule\n2. View a student's balance\n3. List all students\n4. Remove student from list\n5. Quit")
     choice = input("Choose an option: ")
 
     if choice == "1":
@@ -20,6 +20,15 @@ while True:
         for student in students:
             print(student,"-",students[student])
     elif choice == "4":
+        student = input("Enter student name for removal: ")
+        confirm = input(f"Type CONFIRM to remove {student} from token bank: ")
+        if confirm == "CONFIRM":
+            try:
+                students.pop(student)
+                print(f"{student} removed successfully")
+            except KeyError:
+                print("No student on record")
+    elif choice == "5":
         break
     else:
         print("Not a valid option, try again")
